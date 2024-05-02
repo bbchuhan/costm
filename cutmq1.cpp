f3665b84ea1b44bc573b8d2c4c0ff0cdfedd917d
vidhi parikh <vidhi.parikh2001@gmail.com>
2024-05-02 16:16:13 +0530

    coo

---
+++ cutmq1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    
+   for(int row=1;row<=5;row++){
+   	for(int col=1;col<=5;col++){
+   		if( row==1||col==1|| row==5||col==5){
+   			printf("* ");
+		   }else{
+		   	printf("  ");
+		   }
+		   
+	   }
+   	printf("\n");
+   }
+}
+
+
